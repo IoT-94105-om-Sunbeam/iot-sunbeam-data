667ca5bf6eebaa2ce62d921b5cda5e9241eaa253
Ompatil31 <23uet087@dkte.ac.in>
2025-12-12 12:53:45 +0530

    practice day 1

---
+++ iit_iot_internship/arithmetic.py
@@ -0,0 +1,10 @@
+num1=int(input("enter a number"))
+num2=int(input("enter 2nd number"))
+add=num1+num2
+sub=num1-num2
+mul=num1*num2
+div=num1/num1
+print("addition",add)
+print("subtraction",sub)
+print("mul",mul)
+print("division",div)
---
+++ iit_iot_internship/swapping.py
@@ -0,0 +1,7 @@
+num1=int(input("enter a number"))
+num2=int(input("enter 2nd number"))
+temp=num1
+num1=num2
+num1=temp
+print("enter number 1:",+num2)
+print("ENTER 2ND NUMBER:",+num1)
